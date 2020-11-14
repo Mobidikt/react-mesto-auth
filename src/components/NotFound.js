7a7b56e1e203b0bb51e21d0e6291eb5118dbe20c
Kirill Mecker <ghost3299@yandex.ru>
2020-11-14 12:39:14 +0400

    feat: refactoring css

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -5,7 +5,7 @@ import {ROUTES_MAP} from '../utils/routesMap';
 function NotFound ({login}) {
     return (<div className='notFound'>
         <h1 className='notFound__title' >404</h1> 
-        <p className='notFound__text'>Здесь ничего нет. <Link className='notFound__link' to={login ? ROUTES_MAP.MAIN : ROUTES_MAP.SIGN_IN}>Вернуться</Link>.</p>
+        <p className='notFound__text'>Здесь ничего нет. <Link className='link' to={login ? ROUTES_MAP.MAIN : ROUTES_MAP.SIGN_IN}>Вернуться</Link>.</p>
     </div>);
 };
 export default NotFound;
